dcdc89186fe1e89088e60e131be5e588c2acf55f
Firdig Alfalakhi <Alfalakhie@gmail.com>
2025-12-09 17:54:12 +0700

    feat: implement genre selection functionality in Genres component

--- src/components/Genres.tsx
+++ src/components/Genres.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Carousel,
   CarouselContent,
@@ -6,8 +8,21 @@ import {
   CarouselPrevious,
 } from "@/components/ui/Carousel";
 import { GENRES } from "@/constants/genres";
+import { currentYear } from "@/constants/years";
+import { useRouter } from "next/navigation";
 
 export default function Genres() {
+  const router = useRouter();
+  const handleGenre = (genre: string) => {
+    const filters = {
+      year: String(currentYear),
+      sort: "POPULARITY_DESC",
+      genres: genre,
+    };
+
+    const params = new URLSearchParams(filters).toString();
+    router.push(`search?${params.toString()}`);
+  };
   return (
     <Carousel className="px-11">
       {/* Content */}
@@ -15,8 +30,9 @@ export default function Genres() {
         {GENRES.map((item) => (
           <CarouselItem
             key={item.label}
-            className="rounded-sm py-2 px-7 text-xs bg-secondary/75"
+            className="bg-secondary/75 rounded-sm px-7 py-2 text-xs"
             noBasis={true}
+            onClick={() => handleGenre(item.value)}
           >
             {item.value}
           </CarouselItem>
