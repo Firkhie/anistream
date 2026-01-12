fe9f6a9ae1f968739e2f69dc9e58884c3062a830
Firdig Alfalakhi <Alfalakhie@gmail.com>
2026-01-12 13:45:34 +0700

    feat: add characters content in detail page

--- src/components/detail-content/CharactersContent.tsx
+++ src/components/detail-content/CharactersContent.tsx
@@ -1,65 +1,108 @@
 import useAnimeCharactersById from "@/hooks/useAnimeCharactersById";
+import { MicVocal } from "lucide-react";
 import Image from "next/image";
 import { useParams } from "next/navigation";
+import { useState } from "react";
+import Select from "react-select";
+
+type LanguageOption = {
+  value: string;
+  label: string;
+};
+
+const languageOptions: LanguageOption[] = [
+  { value: "japanese", label: "Japanese" },
+  { value: "english", label: "English" },
+  { value: "indonesia", label: "Indonesia" },
+];
 
 export default function CharactersContent() {
   const { slug } = useParams();
   const { data, loading } = useAnimeCharactersById({ id: slug as string });
 
+  const [selectedLang, setSelectedLang] = useState<LanguageOption | null>(languageOptions[0]);
+
   if (loading) return <div>Loading...</div>;
   if (!data?.results?.length) return <div>Characters not found.</div>;
 
   return (
-    <div className="grid grid-cols-[repeat(auto-fill,minmax(398px,1fr))] gap-2">
-      {data.results.map((char) => {
-        const actor = char.voiceActors?.[0] ?? null;
-        const charName = char.name?.userPreferred ?? char.name?.full ?? "-";
+    <div className="flex flex-col gap-3">
+      {/* Header */}
+      <div className="flex justify-between gap-2">
+        <div className="flex items-center gap-x-2">
+          <MicVocal className="h-5 w-5" />
+          <h4>Characters & Voice Actors</h4>
+        </div>
 
-        return (
-          <div
-            key={char.id}
-            className="bg-secondary/75 flex h-24 justify-between rounded-sm text-xs sm:text-[13px]"
-          >
-            {/* Character Info */}
-            <div className="relative flex gap-x-1">
-              <div className="relative aspect-2/3 h-24">
-                <Image
-                  src={char.image ?? "/assets/not-found.png"}
-                  alt={char.id}
-                  sizes="(max-width: 768px) 100vw, 50vw"
-                  fill
-                  className="rounded-l-sm"
-                  unoptimized
-                />
-              </div>
-              <div className="flex flex-col justify-between p-2">
-                <p>{charName}</p>
-                <p className="text-muted-foreground font-extralight">{char.role ?? "-"}</p>
-              </div>
-            </div>
+        <Select
+          placeholder="Select language"
+          options={languageOptions}
+          value={selectedLang}
+          onChange={(option) => option && setSelectedLang(option)}
+          className="my-react-select-container"
+          classNamePrefix="my-react-select"
+        />
+      </div>
 
-            {/* Voice Actor Info */}
-            {actor && (
-              <div key={actor.id} className="relative flex gap-x-1">
-                <div className="flex flex-col items-end justify-between p-2">
-                  <p className="text-end">{actor.name?.userPreferred || actor.name?.full || "-"}</p>
-                  <p className="text-muted-foreground font-extralight">{actor.language ?? "-"}</p>
-                </div>
+      {/* Characters */}
+      <div className="grid grid-cols-[repeat(auto-fill,minmax(398px,1fr))] gap-2">
+        {data.results.map((char) => {
+          const charName = char.name?.userPreferred ?? char.name?.full ?? "-";
+
+          const actor = selectedLang
+            ? char.voiceActors?.find((act) => act.language?.toLowerCase() === selectedLang.value)
+            : null;
+
+          return (
+            <div
+              key={char.id}
+              className="bg-secondary/75 flex h-24 justify-between rounded-sm text-xs sm:text-[13px]"
+            >
+              {/* Character Info */}
+              <div className="relative flex gap-x-1">
                 <div className="relative aspect-2/3 h-24">
                   <Image
-                    alt={actor.id}
-                    src={actor.image ?? "/assets/not-found.png"}
+                    src={char.image ?? "/assets/not-found.png"}
+                    alt={charName}
                     fill
-                    className="rounded-r-sm"
                     sizes="(max-width: 768px) 100vw, 50vw"
+                    className="rounded-l-sm object-cover object-center"
                     unoptimized
                   />
                 </div>
+
+                <div className="flex flex-col justify-between p-2">
+                  <p>{charName}</p>
+                  <p className="text-muted-foreground font-extralight">{char.role ?? "-"}</p>
+                </div>
               </div>
-            )}
-          </div>
-        );
-      })}
+
+              {/* Voice Actor Info */}
+              {actor && (
+                <div className="relative flex gap-x-1">
+                  <div className="flex flex-col items-end justify-between p-2">
+                    <p className="text-end">
+                      {actor.name?.userPreferred ?? actor.name?.full ?? "-"}
+                    </p>
+                    <p className="text-muted-foreground font-extralight">{actor.language ?? "-"}</p>
+                  </div>
+
+                  <div className="relative aspect-2/3 h-24">
+                    <Image
+                      src={actor.image ?? "/assets/not-found.png"}
+                      alt={actor.name?.full ?? "Voice Actor"}
+                      fill
+                      sizes="(max-width: 768px) 100vw, 50vw"
+                      className="rounded-r-sm object-cover object-center"
+                      unoptimized
+                    />
+                  </div>
+                </div>
+              )}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
