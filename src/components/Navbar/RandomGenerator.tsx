4004718121ecf36ae318d5736df73003c8aeb86a
Firdig Alfalakhi <Alfalakhie@gmail.com>
2026-01-21 09:34:30 +0700

    feat: add toaster for anime random generator

--- src/components/Navbar/RandomGenerator.tsx
+++ src/components/Navbar/RandomGenerator.tsx
@@ -4,6 +4,7 @@ import { Shuffle } from "lucide-react";
 import { Button } from "../ui/Button";
 import { useRouter } from "next/navigation";
 import getAnilistId from "@/lib/getAnilistId";
+import { toast } from "sonner";
 
 export default function RandomGenerator() {
   const router = useRouter();
@@ -11,7 +12,9 @@ export default function RandomGenerator() {
   const handleRandomAnime = async () => {
     const id = await getAnilistId();
     if (id) router.push(`detail/${id}`);
+    else toast.error("Fetch anime failed.");
   };
+
   return (
     <Button size={"icon"} onClick={handleRandomAnime}>
       <Shuffle />
