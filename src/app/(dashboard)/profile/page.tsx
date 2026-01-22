9d022487fcbe5f8f495a77df855eaaf04e4b4378
Firdig Alfalakhi <Alfalakhie@gmail.com>
2026-01-22 14:11:07 +0700

    feat: add profile page basic structure

--- src/app/(dashboard)/profile/page.tsx
+++ src/app/(dashboard)/profile/page.tsx
@@ -1,3 +1,14 @@
+import { Button } from "@/components/ui/Button";
+import { LogIn } from "lucide-react";
+
 export default function ProfilePage() {
-  return <div>ProfilePage</div>;
+  return (
+    <div className="flex h-full flex-col items-center justify-center gap-4 pt-16 text-center">
+      <span>You need to log in first to access your profile.</span>
+      <Button size="lg" disabled>
+        <LogIn className="h-4 w-4" />
+        Login
+      </Button>
+    </div>
+  );
 }
