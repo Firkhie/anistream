f5e3cd617b5ef6697fe5348fd5b5740678099572
Firdig Alfalakhi <Alfalakhie@gmail.com>
2026-03-06 14:01:15 +0700

    fix: enhance empty state display for history page with gif

--- src/app/(dashboard)/history/page.tsx
+++ src/app/(dashboard)/history/page.tsx
@@ -56,7 +56,7 @@ export default function HistoryPage() {
   };
 
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex h-full flex-col gap-4">
       {/* Header */}
       <div className="z-30 flex flex-col-reverse gap-3 md:flex-row md:items-center md:gap-4">
         <TvMinimalPlay className="hidden h-6 w-6 shrink-0 md:block" />
@@ -81,41 +81,60 @@ export default function HistoryPage() {
         </div>
       </div>
       {/* Content */}
-      <div className="grid grid-cols-[repeat(auto-fill,minmax(254px,1fr))] gap-2 md:gap-4">
-        {episodes.map((eps) => (
-          <div key={eps.id} className="relative flex flex-col gap-2">
-            {/* Image */}
-            <Link href={`/`} className="group relative aspect-video">
-              <Image
-                src={eps.image ?? "/assets/not-found.png"}
-                alt={eps.title ?? ""}
-                fill
-                sizes="(max-width: 768px) 100vw, 50vw"
-                className="rounded-sm object-cover object-center transition-all duration-100 group-hover:blur-xs"
-              />
-              {/* Play icon */}
-              <div className="absolute inset-0 z-20 flex items-center justify-center opacity-0 transition-opacity duration-100 group-hover:opacity-100">
-                <Play className="h-10 w-10 text-white" />
-              </div>
-            </Link>
-            {/* Delete */}
-            <div
-              className="bg-secondary/75 hover:bg-secondary absolute top-1.5 right-1.5 z-20 cursor-pointer rounded-sm p-1"
-              onClick={(e) => {
-                e.stopPropagation();
-                handleDeleteHistory(eps);
-              }}
-            >
-              <X className="h-4 w-4 text-white" />
-            </div>
-            {/* Title */}
-            <div className="flex flex-col gap-0.5">
-              <span className="line-clamp-1 text-sm font-bold uppercase">{eps.animeTitle}</span>
-              <p className="text-muted-foreground text-xs">Episode {eps.episode}</p>
-            </div>
+      {episodes.length === 0 ? (
+        <div className="flex h-full flex-col items-center justify-center gap-y-2">
+          <video
+            autoPlay
+            loop
+            muted
+            playsInline
+            src="/assets/miyabi.mp4"
+            className="w-full max-w-72 rounded-md"
+          />
+          <div className="text-center text-sm">
+            <span className="font-semibold">No no no... what is this empty list?</span>
+            <p className="font-light text-[hsl(var(--muted-foreground))]">
+              Go watch something before I get *seriously* disappointed.
+            </p>
           </div>
-        ))}
-      </div>
+        </div>
+      ) : (
+        <div className="grid grid-cols-[repeat(auto-fill,minmax(254px,1fr))] gap-2 md:gap-4">
+          {episodes.map((eps) => (
+            <div key={eps.id} className="relative flex flex-col gap-2">
+              {/* Image */}
+              <Link href={`/`} className="group relative aspect-video">
+                <Image
+                  src={eps.image ?? "/assets/not-found.png"}
+                  alt={eps.title ?? ""}
+                  fill
+                  sizes="(max-width: 768px) 100vw, 50vw"
+                  className="rounded-sm object-cover object-center transition-all duration-100 group-hover:blur-xs"
+                />
+                {/* Play icon */}
+                <div className="absolute inset-0 z-20 flex items-center justify-center opacity-0 transition-opacity duration-100 group-hover:opacity-100">
+                  <Play className="h-10 w-10 text-white" />
+                </div>
+              </Link>
+              {/* Delete */}
+              <div
+                className="bg-secondary/75 hover:bg-secondary absolute top-1.5 right-1.5 z-20 cursor-pointer rounded-sm p-1"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handleDeleteHistory(eps);
+                }}
+              >
+                <X className="h-4 w-4 text-white" />
+              </div>
+              {/* Title */}
+              <div className="flex flex-col gap-0.5">
+                <span className="line-clamp-1 text-sm font-bold uppercase">{eps.animeTitle}</span>
+                <p className="text-muted-foreground text-xs">Episode {eps.episode}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
